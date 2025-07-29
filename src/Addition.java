4e3e8aada7dc8d2c28af79b9696fddfb5749f311
rickstine <gowravkumar8@gmail.com>
2025-07-29 12:56:57 +0530

    initial commit

---
+++ src/Addition.java
@@ -0,0 +1,7 @@
+public class Addition {
+    public static void main (String [] args){
+        int a =20 ,b=30, c ;
+        c= a+b;
+        System.out.println("result is = "+c);
+    }
+}
---
+++ src/conditions.java
@@ -0,0 +1,9 @@
+public class conditions {
+    public static void main (String [] args){
+        int a =10;
+        if( a % 2 == 0)
+            System.out.println(a + " is a even number");
+        else
+            System.out.println(a + " is not a even number");
+    }
+}
