863d0676c3506a4e19202838e0ecdd4346ecc66c
rickstine <gowravkumar8@gmail.com>
2025-08-07 16:55:32 +0530

    sample code to overload a function

---
+++ src/FunctionOverloading.java
@@ -0,0 +1,20 @@
+public class FunctionOverloading {
+    static int square ( int x)
+    {
+        return x * x;
+
+    }
+    static double square ( double x)
+    {
+        return x * x;
+    }
+
+    public static void main ( String [] args)
+    {
+        int y = FunctionOverloading.square(10);
+        double z = FunctionOverloading.square(12.12);
+        System.out.println("square of 10 is " + y);
+        System.out.println("square of 12.12 is " + z);
+
+    }
+}
